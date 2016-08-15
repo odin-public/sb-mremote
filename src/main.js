c508dd065d2e806d9a25c2d9af6aabae39632e23
Paul <gear54rus@gmail.com>
2016-08-15 14:16:14 +0700

    Updated configs (07.08.2016).

--- src/main.js
+++ src/main.js
@@ -4,69 +4,163 @@ function capitalize(string) {
 }
 var xmlItem = '{"Name":"","Type":"Container","Expanded":"False","Descr":"","Icon":"mRemoteNG","Panel":"General","Username":"","Domain":"","Password":"","Hostname":"","Protocol":"SSH2","PuttySession":"Default Settings","Port":"22","ConnectToConsole":"False","UseCredSsp":"True","RenderingEngine":"IE","ICAEncryptionStrength":"EncrBasic","RDPAuthenticationLevel":"NoAuth","LoadBalanceInfo":"","Colors":"Colors16Bit","Resolution":"FitToWindow","AutomaticResize":"True","DisplayWallpaper":"False","DisplayThemes":"False","EnableFontSmoothing":"False","EnableDesktopComposition":"False","CacheBitmaps":"True","RedirectDiskDrives":"False","RedirectPorts":"False","RedirectPrinters":"False","RedirectSmartCards":"False","RedirectSound":"DoNotPlay","RedirectKeys":"False","Connected":"False","PreExtApp":"","PostExtApp":"","MacAddress":"","UserField":"","ExtApp":"","VNCCompression":"CompNone","VNCEncoding":"EncHextile","VNCAuthMode":"AuthVNC","VNCProxyType":"ProxyNone","VNCProxyIP":"","VNCProxyPort":"0","VNCProxyUsername":"","VNCProxyPassword":"","VNCColors":"ColNormal","VNCSmartSizeMode":"SmartSAspect","VNCViewOnly":"False","RDGatewayUsageMethod":"Never","RDGatewayHostname":"","RDGatewayUseConnectionCredentials":"Yes","RDGatewayUsername":"","RDGatewayPassword":"","RDGatewayDomain":"","InheritCacheBitmaps":"False","InheritColors":"False","InheritDescription":"False","InheritDisplayThemes":"False","InheritDisplayWallpaper":"False","InheritEnableFontSmoothing":"False","InheritEnableDesktopComposition":"False","InheritDomain":"False","InheritIcon":"False","InheritPanel":"False","InheritPassword":"False","InheritPort":"False","InheritProtocol":"False","InheritPuttySession":"False","InheritRedirectDiskDrives":"False","InheritRedirectKeys":"False","InheritRedirectPorts":"False","InheritRedirectPrinters":"False","InheritRedirectSmartCards":"False","InheritRedirectSound":"False","InheritResolution":"False","InheritAutomaticResize":"False","InheritUseConsoleSession":"False","InheritUseCredSsp":"False","InheritRenderingEngine":"False","InheritUsername":"False","InheritICAEncryptionStrength":"False","InheritRDPAuthenticationLevel":"False","InheritLoadBalanceInfo":"False","InheritPreExtApp":"False","InheritPostExtApp":"False","InheritMacAddress":"False","InheritUserField":"False","InheritExtApp":"False","InheritVNCCompression":"False","InheritVNCEncoding":"False","InheritVNCAuthMode":"False","InheritVNCProxyType":"False","InheritVNCProxyIP":"False","InheritVNCProxyPort":"False","InheritVNCProxyUsername":"False","InheritVNCProxyPassword":"False","InheritVNCColors":"False","InheritVNCSmartSizeMode":"False","InheritVNCViewOnly":"False","InheritRDGatewayUsageMethod":"False","InheritRDGatewayHostname":"False","InheritRDGatewayUseConnectionCredentials":"False","InheritRDGatewayUsername":"False","InheritRDGatewayPassword":"False","InheritRDGatewayDomain":"False"}',
   configs = {
+    'demo-dp4': {
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'demo-dp5': {
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'demo-dp6': {
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'demo-dp7': {
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'demo-dp8': {
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'demo-summit-2015': {
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'demo-summit-2015-office': {
+      'dns': ['dns'],
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
     'lin-ct-clean': {},
     'lin-ct7-clean': {},
-    'lin-mn': {
-      'ep': ['endpoint']
-    },
-    'lin-pba': {
-      'ep': ['endpoint'],
-      'pba': ['pba']
-    },
     'lin-default': {
-      'ep': ['endpoint'],
-      'pim': ['pim'],
-      'mysql': ['mysql'],
       'dns': ['dns'],
-      'ng': ['lsh']
+      'ep': ['endpoint'],
+      'mysql': ['mysql'],
+      'ng': ['lsh'],
+      'pim': ['pim']
     },
     'lin-exch2010': {
-      'cp': ['cp'],
-      'pba': ['pba'],
-      'ep': ['endpoint'],
-      'mysql': ['mysql'],
-      'dns': ['dns'],
-      'ng': ['lsh'],
       'ad1': ['ad1', 'rdp'],
+      'cp': ['cp'],
+      'dns': ['dns'],
+      'ep': ['endpoint'],
       'iis': ['iis', 'rdp'],
+      'mysql': ['mysql'],
+      'ng': ['lsh'],
+      'pba': ['pba'],
+      'wpe': ['wpe', 'rdp']
+    },
+    'lin-exch2013': {
+      'ad1': ['ad1', 'rdp'],
+      'cp': ['cp'],
+      'dns': ['dns'],
+      'ep': ['endpoint'],
+      'iis': ['iis', 'rdp'],
+      'mysql': ['mysql'],
+      'ng': ['lsh'],
+      'pba': ['pba'],
       'wpe': ['wpe', 'rdp']
     },
     'lin-iis': {
+      'ad1': ['ad1', 'rdp'],
       'dns': ['dns'],
       'ep': ['endpoint'],
-      'ad1': ['ad1', 'rdp'],
       'iis71': ['iis71', 'rdp'],
       'wpe': ['wpe', 'rdp']
     },
+    'lin-mn': {
+      'ep': ['endpoint']
+    },
     'lin-mn-for-aps1.x': {
+      'ep': ['endpoint'],
+      'pba': ['pba']
+    },
+    'lin-mn-new': {
+      'ep': ['endpoint']
+    },
+    'lin-mn-nextcp': {
+      'ep': ['endpoint']
+    },
+    'lin-mn-pba-office365-pups-owncloud': {
       'ep': ['endpoint'],
       'pba': ['pba']
     },
     'lin-mn-spconfig': {
       'ep': ['endpoint']
     },
+    'lin-paci': {
+      'ep': ['endpoint']
+    },
+    'lin-paci-mocks': {
+      'ep': ['endpoint']
+    },
+    'lin-paci-pba': {
+      'ep': ['endpoint'],
+      'pba': ['pba']
+    },
+    'lin-pba': {
+      'ep': ['endpoint'],
+      'pba': ['pba']
+    },
     'lin-pba-lsh': {
-      'cp': ['cp'],
-      'pba': ['pba'],
+      'dns': ['dns'],
       'ep': ['endpoint'],
       'mysql': ['mysql'],
+      'ng': ['lsh'],
+      'pba': ['pba'],
+      'pim': ['pim']
+    },
+    'lin-pba-lsh-spconfig': {
       'dns': ['dns'],
-      'store': ['store'],
-      'pbadb': ['pbadb']
+      'ep': ['endpoint'],
+      'mysql': ['mysql'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'lin-pba-new': {
+      'ep': ['endpoint'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
     },
     'lin-qmail': {
-      'cp': ['cp'],
-      'pba': ['pba'],
-      'ep': ['endpoint'],
-      'mysql': ['mysql'],
       'dns': ['dns'],
+      'drweb': ['drweb'],
+      'ep': ['endpoint'],
+      'imp': ['imp'],
+      'mysql': ['mysql'],
       'ng': ['lsh'],
       'pim': ['pim'],
       'qmail': ['qmail'],
-      'imp': ['imp'],
-      'spas': ['spamassassin'],
-      'drweb': ['drweb']
+      'spas': ['spamassassin']
     },
+    'o365-ng-qmail': {
+      'cp': ['cp'],
+      'dns': ['dns'],
+      'mysql': ['mysql'],
+      'pba': ['pba'],
+      'pbadb': ['pbadb'],
+      'store': ['store']
+    },
+    'phoenix65-demodp': {},
     'win-mn': {
       'win-mn': ['mn', 'rdp']
     }
